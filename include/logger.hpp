37aad69e94222599bb2d2cde7c28ce202714561e
Patrick Lindenberg <heapdump@icloud.com>
2020-12-19 15:06:32 +0100

    fix logger issues and add caller function name to logmessage
    Signed-off-by: Patrick Lindenberg <heapdump@icloud.com>

--- include/logger.hpp
+++ include/logger.hpp
@@ -9,11 +9,11 @@ will print something like
 
 	[0.123 debug myfile.cpp:45] error: 67
 */
-#define TRACE(format, ...) rack::logger::log(rack::logger::TRACE_LEVEL, __FUNCTION__, __LINE__, format, ##__VA_ARGS__)
-#define DEBUG(format, ...) rack::logger::log(rack::logger::DEBUG_LEVEL, __FILE__, __LINE__, format, ##__VA_ARGS__)
-#define INFO(format, ...) rack::logger::log(rack::logger::INFO_LEVEL, __FILE__, __LINE__, format, ##__VA_ARGS__)
-#define WARN(format, ...) rack::logger::log(rack::logger::WARN_LEVEL, __FILE__, __LINE__, format, ##__VA_ARGS__)
-#define FATAL(format, ...) rack::logger::log(rack::logger::FATAL_LEVEL, __FILE__, __LINE__, format, ##__VA_ARGS__)
+#define TRACE(format, ...) rack::logger::log(rack::logger::TRACE_LEVEL, __FILE__, __LINE__, __FUNCTION__, format, ##__VA_ARGS__)
+#define DEBUG(format, ...) rack::logger::log(rack::logger::DEBUG_LEVEL, __FILE__, __LINE__, __FUNCTION__, format, ##__VA_ARGS__)
+#define INFO(format, ...) rack::logger::log(rack::logger::INFO_LEVEL, __FILE__, __LINE__, __FUNCTION__, format, ##__VA_ARGS__)
+#define WARN(format, ...) rack::logger::log(rack::logger::WARN_LEVEL, __FILE__, __LINE__, __FUNCTION__, format, ##__VA_ARGS__)
+#define FATAL(format, ...) rack::logger::log(rack::logger::FATAL_LEVEL, __FILE__, __LINE__, __FUNCTION__, format, ##__VA_ARGS__)
 
 
 namespace rack {
@@ -37,7 +37,7 @@ void destroy();
 /** Do not use this function directly. Use the macros above.
 Thread-safe, meaning messages cannot overlap each other in the log.
 */
-void log(Level level, const char *filename, int line, const char *format, ...);
+void log(Level level, const char *filename, int line, const char *funct, const char *format, ...);
 
 
 } // namespace logger
--- src/logger.cpp
+++ src/logger.cpp
@@ -56,7 +56,7 @@ static const int levelColors[] = {
 };
 
 
-static void logVa(Level level, const char *filename, int line, const char *format, va_list args) {
+static void logVa(Level level, const char *filename, int line, const char *funct, const char *format, va_list args) {
     std::lock_guard<std::mutex> lock(logMutex);
 
     auto nowTime = std::chrono::high_resolution_clock::now();
@@ -64,26 +64,26 @@ static void logVa(Level level, const char *filename, int line, const char *forma
 
     if (outputConsole) {
         fprintf(outputConsole, "\x1B[%dm", levelColors[level]);
-        fprintf(outputConsole, "\x1B[0m");
-        fprintf(outputConsole, "[%00006.03f %-26s %4d] %s ", duration / 1000.0, filename, line, levelLabels[level]);
+        fprintf(outputConsole, "[%00006.03f %s::%s:%4d] %s ", duration / 1000.0, filename, funct, line, levelLabels[level]);
         vfprintf(outputConsole, format, args);
+        fprintf(outputConsole, "\x1B[0m");
         fprintf(outputConsole, "\n");
         fflush(outputConsole);
     }
 
-    fprintf(outputFile, "[%00006.03f %-26s %4d] %s ", duration / 1000.0, filename, line, levelLabels[level]);
+    fprintf(outputFile, "[%00006.03f %s::%s:%4d] %s ", duration / 1000.0, filename, funct, line, levelLabels[level]);
     vfprintf(outputFile, format, args);
     fprintf(outputFile, "\n");
     fflush(outputFile);
 }
 
 
-void log(Level level, const char *filename, int line, const char *format, ...) {
+void log(Level level, const char *filename, int line, const char *funct, const char *format, ...) {
     if (logLevel > level) return;
 
     va_list args;
     va_start(args, format);
-    logVa(level, filename, line, format, args);
+    logVa(level, filename, line, funct, format, args);
     va_end(args);
 }
 
